feb38646ca634c7ebd67966ac987fbfa92a0af6e
jonathan <jonathan.grill@goldmarie-finanzen.de>
2026-04-23 18:03:03 +0200

    model: Added HKCategoryTypeIdentifierRegistry and CATEGORICAL_IDENTIFIER_MAPS to __all__.

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,16 @@
 """Module for model definition."""
 
+from types import MappingProxyType
+
 from .base import HK_GROUPS
+from .category_types import HKCategoryTypeIdentifierRegistry
+
+CATEGORICAL_IDENTIFIER_MAPS: MappingProxyType[str, dict[str, int]] = MappingProxyType(
+    {k: v.category_values() for k, v in HKCategoryTypeIdentifierRegistry.items()}
+)
 
 __all__ = [
     "HK_GROUPS",
+    "HKCategoryTypeIdentifierRegistry",
+    "CATEGORICAL_IDENTIFIER_MAPS",
 ]
