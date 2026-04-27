ea13ad4808c1edcdf9c92b843b7bc36cb1d9cc8c
jonathan <jonathan.grill@goldmarie-finanzen.de>
2026-04-27 11:46:46 +0200

    update(import_to_redis): Configured logging from inside container.

--- scripts/import_to_redis.py
+++ scripts/import_to_redis.py
@@ -1,9 +1,31 @@
 """Import Apple Health Export Data to Redis."""
 
+import datetime
+import logging
+
 from src.connection import redis_connect
 from src.importer import HealthDataImporter
 
 if __name__ == "__main__":
+    # configure logging
+    logger = logging.getLogger(__name__)
+
+    warnings_logger = logging.getLogger("py.warnings")
+    logging.captureWarnings(capture=True)
+
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    logging.basicConfig(
+        filename=f"/home/health/output/{now}_report.log",
+        level=logging.INFO,
+        force=True,
+        format="%(asctime)s %(name)s[%(process)d] %(levelname)s %(message)s",
+    )
+
     r = redis_connect(tls=True)
     if r.ping():
-        HealthDataImporter(connection=r).etl(write_feather=True)
+        try:
+            HealthDataImporter(connection=r).etl(write_feather=True)
+            logger.info("Successfully imported Apple Health Export Data.")
+        except Exception as e:  # noqa: BLE001
+            warnings_logger.error(e)
+            logger.info("FAILED to import Apple Health Export Data.")
