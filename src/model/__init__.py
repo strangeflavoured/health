b652bf401e5820c156579db3e1194c13d3456f66
jonathan <jonathan.grill@goldmarie-finanzen.de>
2026-04-27 13:37:14 +0200

    update(model): Added HKMiscTypeIdentifierRegistry to __all__.

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,7 +2,7 @@
 
 from types import MappingProxyType
 
-from .base import HK_GROUPS
+from .base import HK_GROUPS, HKMiscTypeIdentifierRegistry
 from .category_types import HKCategoryTypeIdentifierRegistry
 from .quantity_types import HKQuantityTypeIdentifierRegistry
 
@@ -14,5 +14,6 @@ __all__ = [
     "HK_GROUPS",
     "HKCategoryTypeIdentifierRegistry",
     "HKQuantityTypeIdentifierRegistry",
+    "HKMiscTypeIdentifierRegistry",
     "CATEGORICAL_IDENTIFIER_MAPS",
 ]
