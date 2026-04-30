f77af9174a91b9465cebb4a3615fda433737447e
jonathan <jonathan.grill@goldmarie-finanzen.de>
2026-04-30 20:54:26 +0200

    update(scripts): Added setup_logger for central logging configuration.
    Added explicit connection test with logging.

--- scripts/import_to_redis.py
+++ scripts/import_to_redis.py
@@ -1,34 +1,32 @@
 """Import Apple Health Export Data to Redis."""
 
-import datetime
 import logging
+
+import redis
+from setup_logger import configure_logging
 
 from src.connection import redis_connect
 from src.importer import HealthDataImporter
 
 if __name__ == "__main__":
-    # configure logging
+    configure_logging(__file__)
     logger = logging.getLogger(__name__)
 
-    warnings_logger = logging.getLogger("py.warnings")
-    logging.captureWarnings(capture=True)
-
-    now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    logging.basicConfig(
-        filename=f"/home/health/output/{now}_report.log",
-        level=logging.INFO,
-        force=True,
-        format="%(asctime)s %(name)s[%(process)d] %(levelname)s %(message)s",
-    )
-
     r = redis_connect(tls=True)
-    if r.ping():
-        try:
-            HealthDataImporter(connection=r).etl(write_feather=True)
-            logger.info("Successfully imported Apple Health Export Data.")
-        except Exception as e:  # noqa: BLE001
-            warnings_logger.error(e)
-            if isinstance(e, ExceptionGroup):
-                for exc in e.exceptions:
-                    warnings_logger.error(exc)
-            logger.info("FAILED to import Apple Health Export Data.")
+
+    try:
+        r.ping()
+    except redis.RedisError as e:
+        logger.error(e)
+        logger.warning("FAILED to connect to Redis.")
+        raise e
+
+    try:
+        HealthDataImporter(connection=r).etl(write_feather=True)
+        logger.info("Successfully imported Apple Health Export Data.")
+    except Exception as e:  # noqa: BLE001
+        logger.error(e)
+        if isinstance(e, ExceptionGroup):
+            for exc in e.exceptions:
+                logger.error(exc)
+        logger.warning("FAILED to import Apple Health Export Data.")
