0b488587b1aac02b8152f904f49ed231a0d4932e
jonathan <jonathan.grill@goldmarie-finanzen.de>
2026-04-30 23:22:58 +0200

    update(redis): Added ACL.

--- docker/redis-entrypoint.sh
+++ docker/redis-entrypoint.sh
@@ -12,8 +12,17 @@ openssl pkcs8 \
     -passin "pass:${REDIS_KEY_PASSPHRASE}"
 chmod 600 "$DECRYPTED_KEY"
 
+# Generate ACL file from environment variables at runtime
+ACL_FILE="/run/secrets/users.acl"
+printf 'user default off nopass -@all\n' > "$ACL_FILE"
+printf 'user admin on >%s ~* &* +@all\n' "$ADMIN_PASSWORD" >> "$ACL_FILE"
+printf 'user app on >%s ~HK* resetchannels +ts.add +multi +exec +ping +client|setname +client|getname\n' "$APP_PASSWORD" >> "$ACL_FILE"
+printf 'user insight on >%s ~HK* resetchannels +ts.get +ts.range +ts.revrange +ts.mget +ts.mrange +ts.mrevrange +ts.info +ts.queryindex +ping +info +client|setname +client|getname +scan +type +dbsize\n' "$INSIGHT_PASSWORD" >> "$ACL_FILE"
+printf 'user healthcheck on >%s resetchannels +ping\n' "$HEALTHCHECK_PASSWORD" >> "$ACL_FILE"
+chmod 600 "$ACL_FILE"
+
 # Repoint the key path in REDIS_ARGS to the decrypted location
-REDIS_ARGS="$(echo "$REDIS_ARGS" | sed "s|/certs/${REDIS_SERVER_KEY}|${DECRYPTED_KEY}|g")"
+REDIS_ARGS="$(echo "$REDIS_ARGS" | sed "s|/certs/${REDIS_SERVER_KEY}|${DECRYPTED_KEY}|g") --aclfile ${ACL_FILE}"
 export REDIS_ARGS
 
 # Hand off to the original entrypoint
