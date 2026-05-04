05e9af5ce16eae732f69c75b3b10450d9048d6dd
jonathan <jonathan.grill@goldmarie-finanzen.de>
2026-05-04 16:31:30 +0200

    Update(src/connection): docker_redis_connect takes acl_user as input now to resolve different users.

--- src/connection.py
+++ src/connection.py
@@ -10,21 +10,26 @@ import redis
 logger = logging.getLogger(__name__)
 
 
-def docker_redis_connect() -> redis.Redis:
-    """Connect to redis from inside sandbox container."""
+def docker_redis_connect(acl_user: str = "app") -> redis.Redis:
+    """Connect to redis from inside sandbox container.
+
+    Args:
+        acl_user (str): Account to connect to, defaults to `app`.
+
+    """
     conn_args = {
         "host": os.getenv("REDIS_HOST"),
         "port": int(os.getenv("REDIS_PORT")),
         "db": int(os.getenv("REDIS_DB")),
-        "username": "app",
-        "password": Path("/run/secrets/app_password").read_text().strip(),
+        "username": acl_user,
+        "password": Path(f"/run/secrets/{acl_user}_password").read_text().strip(),
         "decode_responses": True,
     }
 
     mtls_kwargs = {
         "ssl": True,
-        "ssl_certfile": "/run/secrets/app.pem",
-        "ssl_keyfile": "/run/secrets/app.key",
+        "ssl_certfile": f"/run/secrets/{acl_user}.pem",
+        "ssl_keyfile": f"/run/secrets/{acl_user}.key",
         "ssl_ca_certs": "/run/secrets/ca.pem",
         "ssl_check_hostname": True,
         "ssl_cert_reqs": ssl_module.CERT_REQUIRED,
