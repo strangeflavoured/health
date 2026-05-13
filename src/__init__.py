fc11308f5d71fc46e15736525af5ab3f7902ccc4
jonathan <jonathan.grill@goldmarie-finanzen.de>
2026-05-13 18:43:10 +0200

    update(src): Read version from ../versions.json.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,11 @@
 """Main module."""
 
-from ._version import __version__
+import json
+from pathlib import Path
+
 from .importer import HealthDataImporter
+
+with open(Path(__file__).parents[1] / "versions.json") as f:
+    __version__ = json.load(f)["latest"]
 
 __all__ = ["HealthDataImporter", "__version__"]
