e6e99bd1755a4013ff25bee0a0432a69bc9f83a5
Jonathan Grill <40430994+strangeflavoured@users.noreply.github.com>
2026-05-17 15:42:15 +0200

    lint(frontend): Ran prettier.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ['babel-plugin-react-compiler']
-      }
-    })
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
   ],
   server: {
     port: 5173,
