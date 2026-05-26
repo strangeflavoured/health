96768840ad0323d286b4c90ca4da6dc469daa241
Jonathan Grill <40430994+strangeflavoured@users.noreply.github.com>
2026-05-26 20:17:35 +0200

    update(scripts:import_to_redis): Added retry logic
    Signed-off-by: Jonathan Grill <40430994+strangeflavoured@users.noreply.github.com>

--- scripts/import_to_redis.py
+++ scripts/import_to_redis.py
@@ -9,11 +9,12 @@ from src.connection import docker_redis_connect
 from src.importer import HealthDataImporter
 
 if __name__ == "__main__":
+    # set up logger
     configure_logging(__file__)
     logger = logging.getLogger(__name__)
 
+    # test connection
     r = docker_redis_connect()
-
     try:
         r.ping()
     except redis.RedisError as e:
@@ -21,13 +22,19 @@ if __name__ == "__main__":
         logger.warning("FAILED to connect to Redis.")
         raise e
 
-    try:
-        HealthDataImporter(connection=r).etl(write_feather=True)
-        logger.info("Successfully imported Apple Health Export Data.")
+    importer = HealthDataImporter(connection=r)
+    if importer.failures_file.exists():
+        importer.retry_failed()
+        logger.info("Successfully imported previously failed Apple Health data.")
         log_peak_memory(logger)
-    except Exception as e:  # noqa: BLE001
-        logger.error(e)
-        if isinstance(e, ExceptionGroup):
-            for exc in e.exceptions:
-                logger.error(exc)
-        logger.warning("FAILED to import Apple Health Export Data.")
+    else:
+        try:
+            importer.etl(write_feather=True)
+            logger.info("Successfully imported Apple Health Export Data.")
+            log_peak_memory(logger)
+        except Exception as e:  # noqa: BLE001
+            logger.error(e)
+            if isinstance(e, ExceptionGroup):
+                for exc in e.exceptions:
+                    logger.error(exc)
+            logger.warning("FAILED to import Apple Health Export Data.")
