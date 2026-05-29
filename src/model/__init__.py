cab7a2bc15d29b8e9515bccebbe8a32ac8793c01
Jonathan Grill <40430994+strangeflavoured@users.noreply.github.com>
2026-05-29 09:01:09 +0200

    update(src:model): Added HKTypeIdentifierRegistry
    Signed-off-by: Jonathan Grill <40430994+strangeflavoured@users.noreply.github.com>

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,7 @@
 """Module for model definition."""
 
 from types import MappingProxyType
+from typing import Any
 
 from .base import HK_GROUPS, HKMiscTypeIdentifierRegistry
 from .category_types import HKCategoryTypeIdentifierRegistry
@@ -10,8 +11,15 @@ CATEGORICAL_IDENTIFIER_MAPS: MappingProxyType[str, dict[str, int]] = MappingProx
     {k: v.category_values() for k, v in HKCategoryTypeIdentifierRegistry.items()}
 )
 
+HKTypeIdentifierRegistry: MappingProxyType[str, Any] = MappingProxyType(
+    HKCategoryTypeIdentifierRegistry
+    | HKQuantityTypeIdentifierRegistry
+    | HKMiscTypeIdentifierRegistry
+)
+
 __all__ = [
     "HK_GROUPS",
+    "HKTypeIdentifierRegistry",
     "HKCategoryTypeIdentifierRegistry",
     "HKQuantityTypeIdentifierRegistry",
     "HKMiscTypeIdentifierRegistry",
