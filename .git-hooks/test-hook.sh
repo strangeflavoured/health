d7ce542b267ddb1e3759cb67e850292a2cab603b
Jonathan Grill <40430994+strangeflavoured@users.noreply.github.com>
2026-06-17 12:34:35 +0200

    feat(.pre-commit-config): Added tests as pre-push hooks
    Signed-off-by: Jonathan Grill <40430994+strangeflavoured@users.noreply.github.com>

---
+++ .git-hooks/test-hook.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -uo pipefail
+
+ref="${PRE_COMMIT_TO_REF:-HEAD}"
+
+tmp=$(mktemp -d)
+trap 'git worktree remove --force "$tmp" 2>/dev/null; rm -rf "$tmp"' EXIT
+git worktree add --detach --quiet "$tmp" "$ref"
+cd "$tmp" || exit 1
+
+./scripts/compose-wrapper.sh run --build --rm test-runner & test_runner_pid=$!
+./scripts/compose-wrapper.sh run --build --rm scripts-tests & scripts_tests_pid=$!
+./scripts/compose-wrapper.sh run --build --rm bats-tests & bats_tests_pid=$!
+./scripts/compose-wrapper.sh run --build --rm backend-test & backend_test_pid=$!
+./scripts/compose-wrapper.sh run --build --rm frontend-test & frontend_test_pid=$!
+
+wait "$test_runner_pid"; test_runner_rc=$?
+wait "$scripts_tests_pid"; scripts_tests_rc=$?
+wait "$bats_tests_pid"; bats_tests_rc=$?
+wait "$backend_test_pid"; backend_test_rc=$?
+wait "$frontend_test_pid"; frontend_test_rc=$?
+
+[ "$test_runner_rc" -eq 0 ] && [ "$scripts_tests_rc" -eq 0 ] && [ "$bats_tests_rc" -eq 0 ] && [ "$backend_test_rc" -eq 0 ] && [ "$frontend_test_rc" -eq 0 ]
